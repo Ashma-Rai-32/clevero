12287d9cafe777fee2330c8e1b5e63b942162e5a
Ashma-Rai-32 <ashmabantawa32@gmail.com>
2023-06-14 23:40:40 +0545

    get artist from server

--- reactfrontend/src/Home.js
+++ reactfrontend/src/Home.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:null,
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:8081/')
+    .then(res=>{
+      this.setState({data:res.data});
+    console.log("this.state.data",this.state.data);}
+      )
+    .catch(error=>{
+      console.error('Error fetching data',error)}
+      );
+  }
+
   render() {
-    return <div>Home</div>;
+    const {data}=this.state;
+    return (<>
+
+    {this.state.data && this.state.data.map(item=>(<div key ={item.ConstituentId}>{item.DisplayName}</div>))}</>);
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 import express from "express";
-import mysl from "mysql";
+import mysql from "mysql";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
+
+const db=mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:'artdb'
+})
+
+app.get('/',(req,res)=>{
+  const sql ="select * from artist";
+  db.query(sql,(err,result)=>{
+    if(err) return res.json({Message:"Error in server"});
+    return res.json(result);
+  })
+})
+
 app.listen(8081, () => {
   console.log("Listening");
 });
